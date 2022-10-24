bed212e4799fea13d96ec81347fa1c3850c53295
teczap <tpawel890098@o2.pl>
2022-10-25 00:52:42 +0200

    nav+basic_http

--- srv/application.py
+++ srv/application.py
@@ -8,13 +8,21 @@ def login_page():
 
 @app.route("/page", methods=["POST"])
 def main_page():
-    name = request.form.get("name", "nothing")
-    password = request.form.get("pass", "nothing")
+    name = request.form["name"]
+    password = request.form["pass"]
 
     if password=="password" and name=="user":
         return render_template("mainpage.html", name_pass=name)
     else:
         return "Wrong name/pass", 400
+
+@app.route("/simple_text", methods=["GET","POST"])
+def test():
+    return "<h1>works for me</h1>", 200
+
+@app.route("/index", methods=["GET"])
+def index():
+    return render_template("index.html")
 
 if __name__=="__main__":
     app.run()
