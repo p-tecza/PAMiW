1ac79a51a8cc281d1cddc23044f8841ddb9742ce
p-tecza <pawel.tecza.pt@gmail.com>
2022-10-27 01:07:23 +0200

    onchange

--- srv/application.py
+++ srv/application.py
@@ -12,19 +12,37 @@ def main_page():
     password = request.form["pass"]
 
     if password=="password" and name=="user":
-        return render_template("mainpage.html", name_pass=name)
+        return render_template("index.html", name_pass=name)
     else:
         return "Wrong name/pass", 400
+
+
+@app.route("/login_form", methods=["GET"])
+def login_test():
+    return "display_login_form", 200
+
+
+@app.route("/products_file", methods=["GET"])
+def fetch_products():
+    f=open("products.txt","r")
+    content=f.read()
+    return content, 200
 
 @app.route("/simple_text", methods=["GET","POST"])
 def test():
     return "<h1>works for me</h1>", 200
 
-@app.route("/index", methods=["GET"])
+@app.route("/index", methods=["POST", "GET"])
 def index():
-    return render_template("index.html")
+
+    if request.method=="POST":
+        login = request.form["login"]
+        password = request.form["pass"]
+        return render_template("index.html", username=login, greet="user: ")
+    else:
+        return render_template("index.html")
 
 if __name__=="__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
 
 
